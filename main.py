d9eb9713806a8677fbbc459f2fad92309145130a
Yangqi Zheng <yazh3040@atterbom.it.uu.se>
2022-10-12 13:09:51 +0200

    main-v1.2

--- main.py
+++ main.py
@@ -25,22 +25,16 @@ def cards():
         for zero in range(1):
             cards.append((zero, color))
 
-    #72 cards - Type 1-9
+    #72 and 24 cards - Type 1-9 and special
     for _ in range(2):
         for color in colors:
-            for number in range(1, 10):
+            for number in range(1, 13):
                 cards.append((number, color))
-                
-    #16 cards - Type special
-    for _ in range(2):
-        for color in colors:
-            for s in special:
-                cards.append((s, color))
-                
+    
     #8 cards - type unique
     for _ in range(4):
-        for u in unique:
-            cards.append((u, None))
+        for s in unique:
+            cards.append((s, 'none'))
             
     random.shuffle(cards)
     return cards
@@ -73,20 +67,45 @@ green = color.GREEN
 yellow = color.YELLOW
 end = color.ENDC
 
+def whichCard(list, card_number):
+    for n in list:
+        if n == card_number:
+            return n
+        
+def whichColor(list, card_color):
+    for c in list:
+        if c == card_color:
+            return list[c]
+    
+#     if card_color == 'r':
+#         return red
+#     elif card_color == 'b':
+#         return blue
+#     elif card_color == 'g':
+#         return green
+#     else:
+#         return yellow
+    
+def whichSpecial(list, special_card):
+    for s in list:
+        if s == special_card:
+            return list[s]
+        
+
 def showCards(cards):
     number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    color = ['r', 'b', 'g', 'y']
+    special = {10:'sk', 11:'rv', 12:'dr2'}
+    color = {'r':red, 'b':blue, 'g':green, 'y':yellow}
     for card in cards:
-        if card[0] in number and card[1] == 'r':
-            print(f'{red}test{end}')
-        elif card[0] in number and card[1] == 'b':
-            print(f'{blue}test{end}')
-        elif card[0] in number and card[1] == 'g':
-            print(f'{green}test{end}')
-        elif card[0] in number and card[1] == 'y':
-            print(f'{yellow}test{end}')
+        if card[0] in number and card[1] in color:
+            Card(whichColor(color, card[1]), whichCard(number, card[0]))
+        elif card[0] in special and card[1] in color:
+            Card(whichColor(color, card[1]), whichSpecial(special, card[0]))
         else:
             print('Coming soon')
+            
+def Card(color, number):
+    print(f'{color}{number}{end}')
 
 card_pile = cards()
 main()
